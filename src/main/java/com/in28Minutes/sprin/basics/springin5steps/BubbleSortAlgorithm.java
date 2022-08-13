b18a4d10d5413a51c7822402dc1982fc84e347af
pavan7494 <pavanshinde7494@gmail.com>
2022-08-13 19:55:16 +0530

    Created Components and Autowiring

--- src/main/java/com/in28Minutes/sprin/basics/springin5steps/BubbleSortAlgorithm.java
+++ src/main/java/com/in28Minutes/sprin/basics/springin5steps/BubbleSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.in28Minutes.sprin.basics.springin5steps;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class BubbleSortAlgorithm implements SortAlgorithm {
 	public int[] sort(int[] numbers) {
 		// Logic for bubble Sort
--- src/main/java/com/in28Minutes/sprin/basics/springin5steps/SpringIn5StepsApplication.java
+++ src/main/java/com/in28Minutes/sprin/basics/springin5steps/SpringIn5StepsApplication.java
@@ -1,17 +1,33 @@
 package com.in28Minutes.sprin.basics.springin5steps;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
+	private static final String BinarySearchImpl = null;
+
+//	Steps to follow ----->
+//	  1.What are Beans ?  
+//	  2.What are dependencies ? 
+//	  3.Where to find beans ? => No Need
+//	  4.Get Instance of Application Context
+//	  5.Get Instance of Bean using Application Context
+//	  
+
 	public static void main(String[] args) {
 
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
-		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
-		System.out.println(result);
+//		Get Instance of Application Context
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
 
-//		SpringApplication.run(SpringIn5StepsApplication.class, args);
+//		Getting instance of BinarySearchImpl
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+
+		int result = binarySearch.binarySearch(new int[] { 10, 12, 16 }, 7);
+
+		System.out.println(result);
 
 	}
 

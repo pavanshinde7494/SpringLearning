4bc3dfc930b63842b523d4e4ba3d7a81048237ad
pavan7494 <pavanshinde7494@gmail.com>
2022-08-15 19:18:05 +0530

    Debugging Spring and Understanding how Spring Works internally

--- src/main/java/com/in28Minutes/sprin/basics/springin5steps/SpringIn5StepsApplication.java
+++ src/main/java/com/in28Minutes/sprin/basics/springin5steps/SpringIn5StepsApplication.java
@@ -7,15 +7,17 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
-	private static final String BinarySearchImpl = null;
-
 //	Steps to follow ----->
 //	  1.What are Beans ?  
 //	  2.What are dependencies ? 
-//	  3.Where to find beans ? => No Need
-//	  4.Get Instance of Application Context
+//	  3.Where to find beans
+//	     (Here Spring automatically scans for beans in package that we have defined at line 1 ,
+//	      and find where main application class is present using @SpringBootApplication annotation(line 7)   	
+//	      This is called as 'Component Scan')? => No Need
+
+//	  4.Get Instance of Application Context -> This maintains all the beans 
 //	  5.Get Instance of Bean using Application Context
-//	  
+
 
 	public static void main(String[] args) {
 
@@ -32,3 +34,14 @@ public class SpringIn5StepsApplication {
 	}
 
 }
+
+//Internally things are executed -> 
+//	1.Identified candidate component class: file 
+//     	[/home/pavan7494/eclipse-workspace/spring-in-5-steps/target/classes/com/in28Minutes/sprin/basics/springin5steps/BinarySearchImpl
+//	2.Identified candidate component class: file 
+//		[/home/pavan7494/eclipse-workspace/spring-in-5-steps/target/classes/com/in28Minutes/sprin/basics/springin5steps/BubbleSortAlgorithm
+//	3.Creating shared instance of singleton bean 'binarySearchImpl'
+//	4.Creating shared instance of singleton bean 'bubbleSortAlgorithm'
+//	5.Finished creating shared instance of singleton bean 'bubbleSortAlgorithm'
+//	5.Autowiring by type from bean name 'binarySearchImpl' via constructor to bean named 'bubbleSortAlgorithm'
+//  6.Finished creating shared instance of singleton bean 'bubbleSortAlgorithm'
